e7d4381c0934beec02654b0d9c8325cdf9b4034a
Sedulik7771 <100373507+Sedulik7771@users.noreply.github.com>
2022-03-15 23:22:01 +0400

    Add files via upload

---
+++ homework2.js
@@ -0,0 +1,58 @@
+
+
+
+let sum = 0;
+           a = +prompt(" ")
+    while (a != 0)  {
+        sum = sum + a % 10;
+        a = Math.floor(a / 10);
+    }
+               console.log(sum);
+
+let a, b, c;
+a = +prompt(" ");
+b = +pr    ompt(" ");
+c = +prompt(" ");
+    if (a + b > c && b + c > a && a +c > b) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+
+ let num = prompt(' '),
+             sum - 0;
+
+              for (let i = 0; i < num.length; i++) {
+    if (x[i] == 9) {
+       count = count + 1;
+    }
+}
+
+console.log(count)
+
+
+let num = +prompt(" ");
+let rev_num = 0;
+while (num > 0) {
+    rev_num = rev_num * 10 + num % 10;
+    Math.floor(num /10);
+}
+console.log(rev_num);
+
+
+5. while (x > 1) {
+    if (x % 2 !== 0) {
+        isPowerOfTwo = false;
+
+        break;
+    } else {
+        x = x / 2;
+        isPowerOfTwo = true;
+    }
+}
+
+console.log(isPowerOfTwo)
+
+
+
